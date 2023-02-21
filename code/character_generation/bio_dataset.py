2efc01ae3f13ea2eb9ae41cf375a00487f7d5c69
skim658 <33474168+seankim658@users.noreply.github.com>
2023-02-20 22:02:20 -0500

    starting training loop code

--- code/character_generation/bio_dataset.py
+++ code/character_generation/bio_dataset.py
@@ -1,9 +1,90 @@
+import torch
+import pandas as pd
+import numpy as np
 from torch.utils.data import Dataset
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 class BioDataset(Dataset):
+    ''' Dataset class for the sample character bios. 
 
-    def __init__(self, control_code, truncate = False, gpt2_type = 'gpt2', max_length = 1024):
+    Attributes
+    ----------
+    tokenizer : GPT2Tokenizer
+        GPT2 Tokenizer.
+    bios : list
+        List of sample bios.
+    bios_count : int
+        Number of sample bios in the dataset.
+    
+    Methods
+    -------
+    __init__(bios, gpt2_type, max_length, truncate, **kwargs): 
+        Constructor.
+    __len__(): 
+        Returns the number of sample bios in the dataset.
+    __getitem__(idx): 
+        Return a specific record from the dataset.
+    '''
+
+    def __init__(self, bios : pd.Series, gpt2_type = 'gpt2', max_length = 1022, truncate = 0, **kwargs):
+        ''' Constructor. 
+
+        Parameters
+        ----------
+        bios : pd.Series
+            Pandas series of sample bios to populate the dataset.
+        gpt2_type : str
+            Gpt2 type to load in from the transformers module.
+        max_length : int
+            Specifies the maximum length of a token sequence after tokenization.
+        truncate : int
+            Whether to truncate dataset to a specific size. 
+        **kwargs : dict, optional
+            Arbitrary keyword arguments. 
+        '''
         
-        self.tokenizer = GPT2Tokenizer.from_pretrained(gpt2_type)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(gpt2_type, **kwargs)
         self.bios = []
+
+        for idx, text in bios.iteritems():
+            
+            if (truncate > 0) and (idx == truncate):
+                break 
+        
+            bio_tokens = self.tokenizer.tokenize(text)
+            if len(bio_tokens) > max_length:
+                is_start = np.random.randint((len(bio_tokens) - max_length))
+                bio_tokens = bio_tokens[is_start:(is_start + max_length)]
+            
+            self.bios.append(torch.Tensor([
+                self.tokenizer.bos_token_id,
+                *self.tokenizer.convert_tokens_to_ids(bio_tokens),
+                self.tokenizer.eos_token_id
+            ]))
+        
+        self.bios_count = len(self.bios)
+    
+    def __len__(self):
+        ''' Returns the number of bios in the dataset. 
+
+        Returns 
+        -------
+        int
+            The number of bios in the dataset. 
+        '''
+        return self.bios_count 
+    
+    def __getitem__(self, idx):
+        ''' Returns the item at the given index. 
+
+        Parameters
+        ----------
+        idx : int 
+            Specified index of the sample to be returned. 
+
+        Returns 
+        -------
+        torch.Tensor
+            Returns bio at a at the specified index. 
+        '''
+        return self.bios[idx]
--- code/character_generation/character_generation.py
+++ code/character_generation/character_generation.py
@@ -1,10 +1,75 @@
 import numpy as np 
 import pandas as pd 
-from transformers import pipeline, set_seed
+import re 
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch 
 from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
+from bio_dataset import BioDataset
 
-# read in data 
+delimiter = re.compile(r'\s+')
+TEST_SET_SIZE = 0.15
+MODEL_TYPE = 'gpt2'
+EPOCHS = 5
+
+cuda = torch.cuda.is_available()
+if cuda:
+    DEVICE = 'cuda'
+    torch.cuda.manual_seed(42)
+
+### read in and prepare data 
 character_bios = pd.read_csv('data/character_bios.csv')
 
+# calculate length of bios
+character_bios['bio_word_count'] = character_bios['bio'].str.split().str.len() 
+
+# tokenize bios into new column in dataframe and then overwrite original bio strings with simplified versions
+tmp = character_bios.bio.str.split(delimiter)
+def notempty(x): return (len(x) > 0)
+tmp = tmp.apply(lambda x : list(filter(notempty, x)))
+character_bios.insert(character_bios.shape[1], 'bio_tokens', tmp)
+character_bios['bio'] = character_bios.bio_tokens.apply(' '.join)
+del tmp 
+
+# filter out bios with too few (<35) or too many words (>80)
+character_bios = character_bios[(character_bios['bio_word_count'] >= 35) & (character_bios['bio_word_count'] <= 80)].reset_index(drop = True)
+
+### create small test set 
+test_set = character_bios.sample(n = int(character_bios.shape[0] * TEST_SET_SIZE), random_state = 8)
+train_set = character_bios.drop(index = test_set.index).copy() 
+test_set.reset_index(drop = True, inplace = True)
+train_set.reset_index(drop = True, inplace = True)
+# check everything worked correctly 
+assert character_bios.shape[0] == (train_set.shape[0] + test_set.shape[0])
+# for the test set only, keep last 15 words in a new column and remove from original bio column
+test_set.insert(test_set.shape[1], 'true_bio_end', test_set.bio_tokens.str[-15:].apply(' '.join))
+test_set.loc['bio'] = test_set.bio_tokens.str[:-15].apply(' '.join)
+
+### train set
+dataset = BioDataset(train_set.bio, gpt2_type = MODEL_TYPE)
+tokenizer = dataset.tokenizer 
+model = GPT2LMHeadModel.from_pretrained(MODEL_TYPE)
+
+### model and packing code
+
+def pack_tensor(new_tensor, packed_tensor, max_seq_len):
+    ''' 
+    '''
+    if pack_tensor is None:
+        return new_tensor, True, None 
+    if new_tensor.size()[1] + packed_tensor.size()[1] > max_seq_len:
+        return packed_tensor, False, new_tensor
+    else:
+        packed_tensor = torch.cat([new_tensor, packed_tensor[:, 1:]], dim = 1)
+        return packed_tensor, True, None 
+
+def train(dataset, model, tokenizer,
+        batch_size = 16, epochs = EPOCHS,
+        learning_rate = 2e-5, max_seq_len = 768,
+        warmup_steps = 5000, gpt2_type = MODEL_TYPE,
+        output_dir = '.', output_prefix = 'character_generation',
+        test_mode = False, save_model_on_epoch = False):
+    
+    # send model to the gpu (if available) and then set model to training mode
+    model = model.to(DEVICE)
+    model.train()
