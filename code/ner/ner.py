2cd2e4d59e80f877186229081f7641e6ae35bb95
skim658 <33474168+seankim658@users.noreply.github.com>
2023-03-27 00:40:10 -0400

    working on buy transaction patterns

--- code/ner/ner.py
+++ code/ner/ner.py
@@ -3,8 +3,8 @@ import json
 import spacy 
 from spacy.matcher import Matcher 
 
-# nlp = spacy.load('en_core_web_sm')
-# matcher = Matcher(nlp.vocab)
+nlp = spacy.load('en_core_web_sm')
+matcher = Matcher(nlp.vocab)
 
 with open('data/knowledge_base/kb.json') as f:
     knowledge_base = json.load(f)
@@ -12,12 +12,36 @@ with open('data/knowledge_base/kb.json') as f:
 # print(knowledge_base)
 # print(knowledge_base.keys())
 
-# lists 
+#### lists 
 items = (knowledge_base['fall_crops'] + knowledge_base['fish'] + knowledge_base['food'] + 
          knowledge_base['minerals'] + knowledge_base['special_crops'] + 
          knowledge_base['spring_crops'] + knowledge_base['summer_crops'])
+items = sorted(items)
 locations = knowledge_base['locations']
+locations = sorted(locations)
 mobs = knowledge_base['mobs']
-print(locations)
-print()
-print(mobs)
+mobs = sorted(mobs)
+
+### patterns 
+buy_patterns = [
+    {'LEMMA': {'IN': ['buy', 'purchase', 'get']}},              # matches on the words 'buy', 'purchase', or 'get' (required)
+    {'POS': 'NUM', 'OP': '?'},                                  # matches if a quantity (decimal) was included (optional)
+    {'LOWER': {'IN': ['new', 'a', 'an', 'some']}, 'OP': '?'},   # matches in case if a player says something general such as 'I would like to buy an [item]' (optional)
+    {'LOWER': {'IN': ['item', 'items']}, 'OP': '?'},            # matches in case if a player says something general such as 'I would like to buy an item' (optional)
+    {'LOWER': {'IN': ['of']}, 'OP': '?'},                       # matches in case if a player says something general such as 'I would like to buy a bowl of fish stew' (optional)
+    {'LEMMA': {'IN': items}}
+]
+
+matcher.add('BUY_PATTERN', [buy_patterns])
+
+# testing
+test_doc = nlp('buy 4 a item of eels')
+for token in test_doc:
+    print(f'Token: \'{token.text}\', Token POS: {token.pos_}, Token lemma: {token.lemma_}')
+matches = matcher(test_doc)
+for match_id, start, end in matches:
+    print('---------------------------------------------')
+    print(f'Match tuple: ({match_id}, {start}, {end})')
+    print(f'Match: \'{test_doc[start:end]}\'')
+    string_id = nlp.vocab.strings[match_id]
+    print(f'String ID: {string_id}')
